f7ccafb9e9976d605588f9acac21a1d8a70b26de
Fernando Gonzalez-Morales <542@holbertonschool.com>
2019-05-06 22:34:19 -0700

    Added file for task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+'''
+Using the JSONPlaceholder API, export to CSV all tasks
+corresponding to a given employee ID.
+USAGE:
+>$ ./1-export_to_CSV.py <NUMERIC_ID>
+>$ bat <NUMERIC_ID>.csv
+"NUMERIC_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+"NUMERIC_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+"NUMERIC_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+    ...
+"NUMERIC_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+"NUMERIC_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+"NUMERIC_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+'''
+
+
+if __name__ == '__main__':
+    from requests import get
+    from sys import argv
+    import csv
+
+    try:
+        URL = "https://jsonplaceholder.typicode.com"
+        ID = argv[1]
+        employee = get(URL + "/users/" + ID).json()
+        name = employee.get('name')
+        all_tasks = get(URL + "/todos?userID=" + ID).json()
+
+        with open(ID + ".csv", 'w') as f:
+            writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+            for task in all_tasks:
+                writer.writerow([ID, name,
+                                 task.get('completed'), task.get('title')])
+    except IndexError:
+        print("USAGE: ./1-export_to_CSV.py <NUMERIC_ID>")
