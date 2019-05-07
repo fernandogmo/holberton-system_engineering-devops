b8b02c47fcde366330465b3cbd8a0b375fbd9d61
Fernando Gonzalez-Morales <542@holbertonschool.com>
2019-05-06 21:55:29 -0700

    Initial commit

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+'''
+Using the JSONPlaceholder API, returns the TODO list
+corresponding to a given employee ID.
+USAGE:
+>$ ./0-gather_data_from_an_API.py <NUMERIC_ID>
+Employee [EMPLOYEE_NAME] is done with tasks(#_DONE_TASKS/TOTAL_#_TASKS):
+     distinctio vitae autem nihil ut molestias quo
+     voluptas quo tenetur perspiciatis explicabo natus
+     aliquam aut quasi
+     veritatis pariatur delectus
+     nemo perspiciatis repellat ut dolor libero commodi blanditiis omnis
+     repellendus veritatis molestias dicta incidunt
+     excepturi deleniti adipisci voluptatem et neque optio illum ad
+     totam atque quo nesciunt
+'''
+
+
+if __name__ == '__main__':
+    from requests import get
+    from sys import argv
+
+    try:
+        URL = "https://jsonplaceholder.typicode.com"
+        ID = argv[1]
+        employee = get(URL + "/users/" + ID).json()
+        name = employee.get('name')
+        all_tasks = get(URL + "/todos?userID=" + ID).json()
+        done_tasks = [task.get('title')
+                      for task in all_tasks if task.get('completed')]
+        print("Employee {} is done with tasks({}/{}):"
+              .format(name, len(done_tasks), len(all_tasks)))
+        print("\t ", "\n\t ".join(done_tasks))
+    except IndexError:
+        print("USAGE: ./0-gather_data_from_an_API.py <NUMERIC_ID>")
