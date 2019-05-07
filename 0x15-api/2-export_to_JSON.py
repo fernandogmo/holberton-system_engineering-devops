154ea2c7421b130f1f6d795bf97581a6b21edc9c
Fernando Gonzalez-Morales <542@holbertonschool.com>
2019-05-06 23:04:29 -0700

    Added file for task 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+'''
+Using the JSONPlaceholder API, export to CSV all tasks
+corresponding to a given employee ID.
+Example usage:
+>$ ./2-export_to_JSON.py 2
+>$ bat 2.json
+{"2": [{"task": "suscipit repellat esse quibusdam voluptatem
+incidunt", "completed": false, "username": "Antonette"}, {"task":
+"distinctio vitae autem nihil ut molestias quo", "completed":
+true, "username": "Antonette"}, {"task": "et itaque necessitatibus
+maxime molestiae qui quas velit", "completed": false, "username":
+"Antonette"}, {"task": "adipisci non ad dicta qui amet quaerat
+    ...
+{"task": "doloremque quibusdam asperiores libero corrupti illum
+qui omnis", "completed": false, "username": "Antonette"}, {"task":
+"totam atque quo nesciunt", "completed": true, "username": "Antonette"}]}
+'''
+
+
+if __name__ == '__main__':
+    from requests import get
+    from sys import argv
+    import json
+
+    try:
+        URL = "https://jsonplaceholder.typicode.com"
+        ID = argv[1]
+        employee = get(URL + "/users/" + ID).json()
+        name = employee.get('name')
+        all_tasks = get(URL + "/todos?userID=" + ID).json()
+
+        with open(ID + ".json", 'w') as f:
+            tasks = [{"task": task.get('title'),
+                      "completed": task.get('completed'),
+                      "username": name} for task in all_tasks]
+            json.dump({ID: tasks}, f)
+    except IndexError:
+        print("USAGE: ./2-export_to_JSON.py <NUMERIC_ID>")
