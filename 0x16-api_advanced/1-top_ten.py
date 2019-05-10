529dbfb9ec8c24957023b83bba8fb957e6797c9a
Fernando Gonzalez-Morales <542@holbertonschool.com>
2019-05-10 00:03:28 -0700

    Fixed WIP 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,15 +5,15 @@ import requests
 
 def top_ten(subreddit):
     ''' TODO '''
-    URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    URL = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     custom_agent = {'User-Agent': 'fernando@api'}
     response = requests.get(URL, headers=custom_agent, allow_redirects=False)
     if response.status_code == 200:
         try:
-            top = response.json().get('data').get('children')
+            top = response.json().get('data').get('children')[:10]
             for t in top:
                 print(t.get('data').get('title'))
         except Exception as e:
-            print('None')
+            print("None")
     else:
         print("None")
