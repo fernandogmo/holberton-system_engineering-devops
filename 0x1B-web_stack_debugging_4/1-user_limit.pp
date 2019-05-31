3cbd8a4d978474ccf5c84a40e8aa8258be744a3c
Fernando Gonzalez-Morales <542@holbertonschool.com>
2019-05-31 07:16:39 +0000

    WIP fixes for advanced task 1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,14 +1,14 @@
-# puppet file to change ulimit for holberton user 
-
+# Change the OS configuration so that it is possible to
+# login with the holberton user and open a file without any error message.
 file {'/etc/security/limits.conf' :
     ensure =>  present
 }
--> exec { 'change_hardLimit' :
-    path    =>  '/bin',
-    command =>  'sed -i --follow-symlinks s/5/10000/ /etc/security/limits.conf'
+-> exec { 'increase-hard-limit' :
+    command =>  'sed -i "/holberton hard/s/5/20000/" /etc/security/limits.conf',
+    path    =>  '/bin'
 }
--> exec { 'change_softLimit' :
-    path    =>  '/bin',
-    command =>  'sed -i --follow-symlinks s/4/200/ /etc/security/limits.conf'
+-> exec { 'increase-soft-limit' :
+    command =>  'sed -i "/holberton soft/s/4/10000/" /etc/security/limits.conf',
+    path    =>  '/bin'
 }
 
