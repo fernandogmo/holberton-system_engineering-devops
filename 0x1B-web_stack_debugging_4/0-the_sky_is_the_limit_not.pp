b7aa3a5c720f5402b89f8fc220116e9590537d04
Fernando Gonzalez-Morales <542@holbertonschool.com>
2019-05-31 07:01:37 +0000

    WIP for task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,19 +1,5 @@
 # https://www.puppetcookbook.com/posts/restart-a-service-when-a-file-changes.html
 exec { 'fix-ULIMIT':
-  command => 'echo "ULIMIT=\'-n 4096\'" > /etc/default/nginx &&  /etc/init.d/nginx restart',
+  command => 'echo "ULIMIT=\'-n 4096\'" > /etc/default/nginx && /etc/init.d/nginx restart',
   path    => '/bin',
-}
-
-service { 'nginx':
-  ensure  => 'running',
-  enable  => true,
-  require => Package['nginx'],
-}
-
-file { '/etc/default/nginx':
-  notify  => Service['nginx'],  # this sets up the relationship
-  mode    => '0600',
-  owner   => 'root',
-  group   => 'root',
-  require => Package['nginx'],
 }
